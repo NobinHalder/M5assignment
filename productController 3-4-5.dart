e95de1318e72c77b0b836f5e72d907186d73fa95
Nobin Halder <nobinhalder100@gmail.com>
2025-11-19 19:43:27 +0300

    Module 14 Assignment

---
+++ productController 3-4-5.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'Model/ProductModel.dart';
+import 'Utils/urls.dart';
+
+class ProductController{
+  List<Data>products =[];
+  bool isLoading = true;
+
+  Future fetchProducts() async {
+    final response = await http.get(Uri.parse(Urls.readProduct));
+
+    if(response.statusCode == 200){
+       isLoading = false;
+      final data = jsonDecode(response.body);
+
+      ProductModel model = ProductModel.fromJson(data);
+
+      products = model.data ?? [];
+
+    }
+  }
+
+  Future<bool>createProduct(Data data) async {
+    final response =await http.post(Uri.parse(Urls.createProduct),
+      headers: {
+      'Content-Type':'application/json'
+      },
+
+      body:jsonEncode(
+
+          {
+            "ProductName": data.productName,
+            "ProductCode": DateTime.now().microsecondsSinceEpoch,
+            "Img": data.img,
+            "Qty": data.qty,
+            "UnitPrice": data.unitPrice,
+            "TotalPrice": data.totalPrice,
+          }
+      ),
+
+    );
+    print(response.body);
+
+    if(response.statusCode == 200 ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+  Future<bool>deleteProduct(String ProductId) async {
+    final response =await http.get(Uri.parse(Urls.deleteProduct(ProductId)));
+
+    print(Urls.deleteProduct(ProductId));
+
+    if(response.statusCode == 200){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
